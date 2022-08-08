27b6a627352c2386c003e7aa951a8a267f085f15
prama pandit <panditprama@gmail.com>
2022-08-08 17:12:19 +0545

    add checkout form

--- src/Components/Pages/Cart.jsx
+++ src/Components/Pages/Cart.jsx
@@ -29,7 +29,7 @@ const Cart = () => {
           <div className=" font-Poppins font-bold text-3xl text-[#0D1222] mb-10">
             Cart Products
           </div>
-          <div className="w-full md:grid grid-cols-12">
+          <div className="w-full md:grid grid-cols-12 gap-10">
             <div className="w-full col-span-8">
               <table className="w-full  ">
                 {Pdata.map((val, index) => {
@@ -37,6 +37,53 @@ const Cart = () => {
                   return <TableRowCart key={val.id} {...val} />;
                 })}
               </table>
+            </div>
+
+            <div className=" col-span-4">
+              <div className="w-full bg-[#F4F4F4] px-5 py-5 flex flex-col gap-5 ">
+                <div className=" text-[#0D1222]  sm:text-lg font-bold font-Mada">
+                  Coupon Code
+                </div>
+                <input
+                  className="bg-white px-4 w-full py-3 text-xl font-bold outline-none focus:outline-none"
+                  type="text"
+                  name=""
+                />
+                <div className="flex justify-end">
+                  <button className="btn-withbg">Check</button>
+                </div>
+
+                <div className="w-full flex justify-between items-center mt-2 ">
+                  <div className="flex flex-col gap-1 ">
+                    <div className="text-[#0D1222]  sm:text-lg font-bold font-Mada">
+                      Subtotal
+                    </div>
+                    <div className="text-[#969696]  sm:text-lg  font-Mada">
+                      4 Product
+                    </div>
+                  </div>
+                  <div className="text-[#0D1222]  sm:text-lg  font-Mada">
+                    $ 160 USD
+                  </div>
+                </div>
+
+                <div className="w-full  border-b-2 border-[#969696] rounded-md"></div>
+
+                <div className="w-full flex justify-between items-center mt-2 ">
+                  <div className="flex flex-col gap-1 ">
+                    <div className="text-[#0D1222]  sm:text-lg font-bold font-Mada">
+                      Total
+                    </div>
+                  </div>
+                  <div className="text-[#0D1222]  sm:text-lg  font-Mada">
+                    $ 160 USD
+                  </div>
+                </div>
+
+                <div className="flex justify-end">
+                  <button className="btn-withbg">Checkout</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
