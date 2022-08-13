49f4a49aed551e8518746397123f2ed044375659
prama pandit <panditprama@gmail.com>
2022-08-13 22:24:08 +0545

    add chandes

--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Shop = () => {
   return (
     <>
-      <div className="w-full w-padding max-w bg-gradient"></div>
+      <div className="w-full w-padding max-w bg-gradient h-screen"></div>
     </>
   );
 };
