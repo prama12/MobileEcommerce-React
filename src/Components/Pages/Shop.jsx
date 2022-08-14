07a172f580af4c30f53b6b5026b903cee25a47d5
prama pandit <panditprama@gmail.com>
2022-08-14 10:54:31 +0545

    Add hero section on shop page

--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -3,7 +3,23 @@ import React from "react";
 const Shop = () => {
   return (
     <>
-      <div className="w-full w-padding max-w bg-gradient h-screen"></div>
+      {/* hero section  */}
+      <div className="w-full h-fit hero-gradient pt-5">
+        <div className="w-full max-w w-padding relative">
+          <div className="w-full grid sm:grid-cols-2 xl:mb-32 mb-5 ">
+            <div className="w-full flex flex-col gap-7">
+              <div className=" font-Mada font-bold text-white text-4xl">
+                Cart
+              </div>
+              <div className=" font-Poppins text-[#F4F4F4] text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget
+                gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus
+                ullamcorper.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
