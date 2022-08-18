3f01f5557a5589defc14b9ad2e708297e842f034
prama pandit <panditprama@gmail.com>
2022-08-18 12:58:13 +0545

    arrange home page hero section

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -20,11 +20,11 @@ const Home = () => {
           <div className="flex flex-col gap-y-10  md:grid grid-cols-12 gap-5 items-center">
             <div className=" w-full col-span-8">
               <div className="w-full grid grid-cols-12 gap-3">
-                <div className="col-span-8 w-full flex flex-col gap-10 text-white">
-                  <div className="text-5xl font-Mada font-bold">
+                <div className="col-span-8 w-full flex flex-col gap-5 sm:gap-10 text-white">
+                  <div className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-Mada font-bold">
                     Get the Latest Dress Models From Us
                   </div>
-                  <div className=" text-lg font-Poppins text-[#DDDDDD]">
+                  <div className=" text-xs  sm:text-sm md:text-lg font-Poppins text-[#DDDDDD]">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
                     eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem
                     finibus ullamcorper.
@@ -112,9 +112,9 @@ const Home = () => {
 
       <div className="hero-gradient w-full h-fit">
         <div className="w-full max-w w-padding">
-          <div className="grid grid-cols-12 gap-10 w-full  ">
-            <div className=" relative flex col-span-7 w-full justify-end px-20 gap-20 ">
-              <div className=" absolute w-52 -left-5  ">
+          <div className="md:grid grid-cols-12 gap-10 w-full  ">
+            <div className=" relative flex col-span-7 w-full justify-end  sm:spx-10 md:px-0 xl:px-10 ">
+              <div className=" absolute w-32 sm:w-40 lg:w-52 md:-left-5 left-0 ">
                 <img className="" src="/Images/cover.png" alt="" />
                 <div className="">
                   <img src="/Images/elipes.png" alt="" />
@@ -122,10 +122,9 @@ const Home = () => {
               </div>
 
               <div className=" flex items-center justify-center">
-                <div className=" shadow hover:shadow-xl  flex justify-center items-center w-10 h-10 bg-white ">
+                <div className=" shadow hover:shadow-xl  flex justify-center items-center w-5 h-5 md:w-10 md:h-10 bg-white ">
                   <svg
-                    width="12"
-                    height="21"
+                    className=" w-7  h-4 "
                     viewBox="0 0 12 21"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -133,23 +132,22 @@ const Home = () => {
                     <path
                       d="M10.3442 1.99998L1.80024 10.544L10.3442 19.088"
                       stroke="#5463FF"
-                      stroke-width="2.4"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2.4"
+                      strokeMiterlimit="10"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                   </svg>
                 </div>
-                <div className="w-60 ">
+                <div className=" w-40 sm:w-48 md:w-60 ">
                   <img className="" src="/Images/cover.png" alt="" />
                   <div className="">
                     <img src="/Images/elipes.png" alt="" />
                   </div>
                 </div>
-                <div className="shadow hover:shadow-xl  flex justify-center items-center w-10 h-10 bg-white ">
+                <div className="shadow hover:shadow-xl  flex justify-center items-center w-5 h-5 md:w-10 md:h-10 bg-white ">
                   <svg
-                    width="12"
-                    height="21"
+                    className=" w-7  h-4 "
                     viewBox="0 0 12 21"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -157,38 +155,42 @@ const Home = () => {
                     <path
                       d="M1.65576 1.99998L10.1998 10.544L1.65576 19.088"
                       stroke="#5463FF"
-                      stroke-width="2.4"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2.4"
+                      strokeMiterlimit="10"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                   </svg>
                 </div>
               </div>
             </div>
             <div className=" col-span-5 py-10 text-white flex flex-col gap-10 ">
-              <div className=" font-Mada text-5xl font-bold">
+              <div className=" font-Mada text-3xl   sm:text-4xl md:text-5xl  text-center md:text-left font-bold">
                 The Oblongers T-Shirt
               </div>
-              <div className=" flex justify-between ">
+              <div className=" flex justify-between items-center ">
                 <div className=" flex gap-2 items-center">
                   <Rating />
                   <div className=" font-Poppins text-sm text-[#DDDDDD] ">
                     ( 20k )
                   </div>
                 </div>
-                <div className=" font-Poppins text-[40px] font-bold">50$</div>
+                <div className=" font-Poppins text-3xl md:text-[40px] font-bold">
+                  50$
+                </div>
               </div>
 
-              <div className="font-Poppins text-sm text-[#DDDDDD] ">
+              <div className="font-Poppins text-xs sm:text-sm text-[#DDDDDD] ">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
                 mollis, justo nec porttitor auctor, erat sapien faucibus lectus,
                 vel tempor dolor augue et lectus. Proin et lacus blandit quam
                 semper placerat.
               </div>
 
-              <div className=" bg-white flex w-fit rounded justify-start ">
-                <button className="btn gradient-text ">Order Now</button>
+              <div className=" flex md:w-fit justify-center ">
+                <div className="w-fit rounded  bg-white">
+                  <button className="btn gradient-text ">Order Now</button>
+                </div>
               </div>
             </div>
           </div>
